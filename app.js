fd4ff30265eb3cbfe1ee93e0cfe160fb7bd09df7
Awesome Inc <awesomeinc@AwesomeIsaacMBP.localdomain>
2018-10-15 13:59:20 -0400

    Styled buttons added some javascript

--- app.js
+++ app.js
@@ -16,11 +16,16 @@ let app = new Vue ({
     el: "#app",
 
     data: {
-
+        turn: 1,
+        color: ['danger', 'primary', 'warning', 'success'],
+        order: []
     },
 
     methods: {
-
+        getRandomButton: function () {
+            let randomButton = Math.floor(Math.random() * this.color.length);
+            if (this.color[randomButton])
+        }
     }
 
 });
