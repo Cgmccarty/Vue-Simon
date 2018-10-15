adf2e701b857f10edc04acf50e0551f9f698715e
Mayme Clayton <mayme.clayton@gmail.com>
2018-10-15 16:29:57 -0400

    unclear what I've changed

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 Vue.component('simonbuttons', {
 
-    props: ['colorclass', 'active'],
+    props: ['colorclass', 'isActive'],
 
     method: {
-      selected: function(item){
-        this.active= item;
-
-      }
 
     },
 
-    template: '<button class="btn" v-on:click="selected(colorclass)" v-bind:class="colorclass">&nbsp</button>'
+    template: '<button class="btn" v-on:click="selected()" v-bind:class="colorclass">&nbsp</button>'
 
 });
 
@@ -20,18 +16,29 @@ let app = new Vue ({
     el: "#app",
 
     data: {
-        turn: 1,
+        //turn: 1,
         color: ['danger', 'primary', 'warning', 'success'],
-        order: [],
-        active: ''
+        sequence: ['danger', 'warning', 'success', 'primary']
+        //order: [],
+        //active: ''
     },
 
     methods: {
-        getRandomButton: function () {
-            let randomButton = Math.floor(Math.random() * this.color.length);
-            this.selected (randomButton);
-        }
+
+        selected: function(item){
+          this.active= item;
+        //getRandomButton: function () {
+            //let randomButton = Math.floor(Math.random() * this.color.length);
+            //this.selected (randomButton);
+        //}
+
+        //start: function(){
+
+
+
+        //}
 
     }
+  }
 
 });
