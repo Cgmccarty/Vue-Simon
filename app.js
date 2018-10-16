86804f7c069f625ecb238c8bcdf6c3bdcb7b4191
Awesome Inc <awesomeinc@AwesomeIsaacMBP.localdomain>
2018-10-16 15:33:58 -0400

    some gameplay with limitations

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ let app = new Vue ({
 
     data: {
 
-        turn: 0,
+        prompt: "We\'re about to start",
+        btnTap: 0,
+        colors: ['red', 'blue', 'yellow', 'green'],
         sequence: [],
-        tapped: ''
+        tapped: '',
+        userTaps: []
 
     },
 
@@ -15,17 +18,54 @@ let app = new Vue ({
         playSequence: function () {
 
             let self = this;
+            self.prompt = 'Pay attention!';
+            let randomButton = Math.floor(Math.random() * 3);
+            self.sequence.push(self.colors[randomButton]);
+
+            let intervalId = setInterval(function () {
+
+                if (self.btnTap < self.sequence.length) {
+                    self.highlight(self.sequence[self.btnTap]);
+                    self.btnTap++;
+                }
+                else {
+                    clearInterval(intervalId);
+                    self.btnTap = 0;
+                    self.prompt = "you try NOW";
+                }
+            }, 1000);
         },
 
-        makeActive: function (color) {
+        highlight: function (color) {
 
             let self = this;
 
             self.tapped = color;
             setTimeout(function() {
-                self.currentButton = '';
-            }, 300);
+                self.tapped = '';
+            }, 500);
+        },
+
+        makeActive: function (color) {
+
+            let self = this;
+            self.userTaps.push(color);
+            self.highlight(color);
+
+
+            if (self.userTaps.join("") === self.sequence.join("")) {
+                self.userTaps = [];
+                self.btnTaps = 0;
+                self.playSequence();
+            }
+            else {
+                self.prompt = 'LOSER!';
+            }
+
+
         }
+
+
     }
 
 });
