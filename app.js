4d8e2a1d2b022e8781d14e6c8143425d1ef0b1fe
Awesome Inc <awesomeinc@AwesomeIsaacMBP.localdomain>
2018-10-16 13:23:35 -0400

    Got buttons to properly highlight

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
-Vue.component('simonbuttons', {
-
-    props: ['colorclass', 'isActive'],
-
-    method: {
-
-        // selected: function () {
-        //   this.isActive = true;
-        // }
-
-    },
-
-    template: '<button class="btn" v-bind:class="colorclass" v-on:click="$parent.selected()">&nbsp</button>'
-
-
-});
-
-
 let app = new Vue ({
 
     el: "#app",
 
     data: {
 
-        turn: 1,
-        color: ['red', 'blue', 'yellow', 'green'],
-        order: [],
-        isActive: '',
-        sequence: ['']
+        turn: 0,
+        sequence: [],
+        tapped: ''
 
     },
 
     methods: {
 
-        getRandomButton: function () {
-            let randomButton = Math.floor(Math.random() * this.color.length);
-            this.selected(this.color[randomButton]);
+        playSequence: function () {
+
+            let self = this;
         },
 
-        selected: function (item) {
-            this.isActive = this.color[item];
-            console.log(this.isActive);
+        makeActive: function (color) {
+
+            let self = this;
+
+            self.tapped = color;
+            setTimeout(function() {
+                self.currentButton = '';
+            }, 300);
         }
     }
-  }
 
 });
